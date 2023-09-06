16ac708377d0c106fde8a15056d51ec8e0d1a65a
artem_pavlov <apavlov@region.komus.net>
2023-09-06 16:08:02 +0300

    3 add 3 tasks

---
+++ hw1/Program.cs
@@ -0,0 +1,54 @@
+﻿// 1
+// Console.Write("Enter first number: ");
+// string StrNumA = Console.ReadLine();
+// int numA = int.Parse(StrNumA);
+
+// Console.Write("Enter second number: ");
+// string StrNumB = Console.ReadLine();
+// int numB = int.Parse(StrNumB);
+
+// if(numA>=numB)
+// {
+//     Console.Write("Max: ");
+//     Console.WriteLine(numA);
+// }
+// else
+// {
+//     Console.Write("Max: ");
+//     Console.WriteLine(numB); 
+// }
+
+
+// 2
+// Console.Write("Enter first number: ");
+// string strNumA = Console.ReadLine();
+// int numA = int.Parse(strNumA);
+
+// Console.Write("Enter second number: ");
+// string strNumB = Console.ReadLine();
+// int numB = int.Parse(strNumB);
+
+// Console.Write("Enter third number: ");
+// string strNumC = Console.ReadLine();
+// int numC = int.Parse(strNumC);
+
+// int max = numA;
+
+// if(numB>=max) max = numB;
+// if(numC>=max) max = numC;
+
+// Console.Write("Max = ");
+// Console.WriteLine(max);
+
+
+// 3
+Console.Write("Enter number: ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+
+if(num % 2 == 0) {
+    Console.WriteLine("Четное");
+}
+else{
+    Console.WriteLine("Нечетное");
+}
