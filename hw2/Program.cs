84aaf2e1326cbad92ec820f383e83b4dfa2b088b
artem_pavlov <apavlov@region.komus.net>
2023-09-07 16:51:27 +0300

    add task 15

--- hw2/Program.cs
+++ hw2/Program.cs
@@ -18,21 +18,42 @@
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-Console.Write("Введите число: ");
+// Console.Write("Введите число: ");
+// string number = Console.ReadLine();
+// int num = int.Parse(number);
+
+// if (num > 99)
+// {
+//     int num1;
+//     while (num > 999)
+//     {
+//         num = num / 10;
+//     }
+//     num1 = num % 10;
+//     System.Console.WriteLine(num1);
+// }
+// else
+// {
+//     System.Console.WriteLine("третьей цифры нет");
+// }
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите число от 1 до 7: ");
 string number = Console.ReadLine();
 int num = int.Parse(number);
 
-if (num > 99)
+if (num < 1 || num > 7)
 {
-    int num1;
-    while (num > 999)
-    {
-        num = num / 10;
-    }
-    num1 = num % 10;
-    System.Console.WriteLine(num1);
+    System.Console.WriteLine("некорректное число");
+}
+else if (num<6)
+{
+    System.Console.WriteLine("нет");
 }
 else
 {
-    System.Console.WriteLine("третьей цифры нет");
+    System.Console.WriteLine("да");
 }
