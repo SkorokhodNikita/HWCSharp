fa0784b3dbaa441176e475deb9203b82faf0dd6c
artem_pavlov <apavlov@region.komus.net>
2023-09-07 16:40:56 +0300

    add task 13

--- hw2/Program.cs
+++ hw2/Program.cs
@@ -1,18 +1,38 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и
 // на выходе показывает вторую цифру этого числа.
 
-Console.Write("Введите трехзначное число: ");
+// Console.Write("Введите трехзначное число: ");
+// string number = Console.ReadLine();
+// int num = int.Parse(number);
+
+// if (num < 100 || num > 999)
+// {
+//     System.Console.WriteLine("Некорректное число");
+// }
+// else
+// {
+//     int num1 = (num % 100) / 10;
+//     System.Console.WriteLine(num1);
+// }
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
 string number = Console.ReadLine();
 int num = int.Parse(number);
 
-if (num < 100 || num > 999)
+if (num > 99)
 {
-    System.Console.WriteLine("Некорректное число");
+    int num1;
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    num1 = num % 10;
+    System.Console.WriteLine(num1);
 }
 else
 {
-    int num1 = (num % 100) / 10;
-    System.Console.WriteLine(num1);
+    System.Console.WriteLine("третьей цифры нет");
 }
-
-
