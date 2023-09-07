c72e626563841d8b4aeb1fa117df559c0035635e
artem_pavlov <apavlov@region.komus.net>
2023-09-07 15:39:07 +0300

    add task 10

---
+++ hw2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и
+// на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число: ");
+string number = Console.ReadLine();
+int num = int.Parse(number);
+
+if (num < 100 || num > 999)
+{
+    System.Console.WriteLine("Некорректное число");
+}
+else
+{
+    int num1 = (num % 100) / 10;
+    System.Console.WriteLine(num1);
+}
+
+
